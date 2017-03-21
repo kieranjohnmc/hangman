f36e4c9ba1f2108d07dbc7d9cd122ab5fa839287
CallumSmith1 <callum.smith000@gmail.com>
2017-03-21 13:31:11 +0000

    Database fix?

--- Database.sql
+++ Database.sql
@@ -11,9 +11,30 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+
+-- Dumping database structure for hangman
+CREATE DATABASE IF NOT EXISTS `hangman` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `hangman`;
+
+-- Dumping structure for table hangman.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `UserID` int(11) NOT NULL AUTO_INCREMENT,
+  `VName` varchar(50) DEFAULT NULL,
+  `Score` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`UserID`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1 COMMENT='Table for users';
+
 -- Dumping data for table hangman.users: ~0 rows (approximately)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+-- Dumping structure for table hangman.words
+CREATE TABLE IF NOT EXISTS `words` (
+  `Words` int(11) NOT NULL AUTO_INCREMENT,
+  `Word` text,
+  `Text` text,
+  PRIMARY KEY (`Words`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1 COMMENT='Table of words';
 
 -- Dumping data for table hangman.words: ~5 rows (approximately)
 /*!40000 ALTER TABLE `words` DISABLE KEYS */;
